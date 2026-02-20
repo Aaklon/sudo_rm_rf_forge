cd2c29408b859bc4c62ef5cdab3f99c4f3ae2416
Aaklon <64139497+Aaklon@users.noreply.github.com>
2026-02-20 20:38:45 +0530

    Update config.js

--- sudo_rm_rf_forge-branch1/src/firebase/config.js
+++ sudo_rm_rf_forge-branch1/src/firebase/config.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Replace with your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyChRrrBOv87yVooPTQgZopzUXpusatr7bE",
-  authDomain: "bookmylibrary-67aef.firebaseapp.com",
-  projectId: "bookmylibrary-67aef",
-  storageBucket: "bookmylibrary-67aef.firebasestorage.app",
-  messagingSenderId: "632586124007",
-  appId: "1:632586124007:web:fdee57d62b797789004042",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
